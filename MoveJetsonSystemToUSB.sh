3d4cd8dde821b764cdc44820aea870f32ed5ac4a
Andre1506 <andrebilk2@googlemail.com>
2022-01-16 00:12:41 +0100

    Update MoveJetsonSystemToUSB.sh

--- MoveJetsonSystemToUSB.sh
+++ MoveJetsonSystemToUSB.sh
@@ -4,13 +4,10 @@
 # Copy the root directory to the given volume
 
 
-
-
 sudo parted -s /dev/$1 "mklabel gpt"
 sudo parted -s /dev/$1 "mkpart primary ext4 1M -1"
 sudo mkfs -t ext4 $(/dev/"$1"1)
-sudo mkdir /mnt/new_root
-sudo mount -t ext4 $(/dev/"$1"1)/mnt/new_root/
-sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/new_root
+sudo mount -t ext4 $(/dev/"$1"1)/mnt/
+sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/
 
 
