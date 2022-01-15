d3611884abd3b5bd0de200322daf7b24e9ed5374
Andre1506 <andrebilk2@googlemail.com>
2022-01-16 00:17:08 +0100

    Update MoveJetsonSystemToUSB.sh

--- MoveJetsonSystemToUSB.sh
+++ MoveJetsonSystemToUSB.sh
@@ -6,8 +6,8 @@
 
 sudo parted -s /dev/$1 "mklabel gpt"
 sudo parted -s /dev/$1 "mkpart primary ext4 1M -1"
-sudo mkfs -t ext4 $(/dev/"$1"1)
-sudo mount -t ext4 $(/dev/"$1"1)/mnt/
+sudo mkfs -t ext4 "/dev/"$1"1"
+sudo mount -t ext4 "/dev/"$1"1"/mnt/
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/
 
 
