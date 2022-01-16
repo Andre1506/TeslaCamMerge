596d9dcdf0d230ac72d1b01e4ac81e788285f884
Andre1506 <andrebilk2@googlemail.com>
2022-01-16 13:45:45 +0100

    Update MoveJetsonSystemToUSB.sh

--- MoveJetsonSystemToUSB.sh
+++ MoveJetsonSystemToUSB.sh
@@ -7,7 +7,7 @@
 sudo parted -s /dev/$1 "mklabel gpt"
 sudo parted -s /dev/$1 "mkpart primary ext4 1M -1"
 sudo mkfs -t ext4 "/dev/"$1"1"
-sudo mount -t ext4 "/dev/"$1"1" /mnt/
+sudo mount -t ext4 -q "/dev/"$1"1" /mnt/
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/
 PARTUUID_STRING=$(sudo blkid -o value -s PARTUUID "/dev/"$1"1")
 BOOT_ORG='root=/dev/mmcblk0p1'
@@ -16,3 +16,4 @@ echo $BOOT_ORG
 echo $BOOT_NEW
 sudo sed -i "s!$BOOT_ORG!$BOOT_NEW!" /boot/extlinux/extlinux.conf
 sudo reboot
+
