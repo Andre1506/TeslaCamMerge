15ee0fe2d87069a7a626c5a107b6f74eaa203678
Andre1506 <83266528+Andre1506@users.noreply.github.com>
2022-01-17 22:05:13 +0100

    Update MoveJetsonSystemToUSB.sh

--- MoveJetsonSystemToUSB.sh
+++ MoveJetsonSystemToUSB.sh
@@ -3,15 +3,21 @@
 # MIT License
 # Move the Jetson OS to USB
 
-
-sudo parted -s /dev/$1 "mklabel gpt"
-sudo parted -s /dev/$1 "mkpart primary ext4 1M -1"
-sudo mkfs -t ext4 "/dev/"$1"1"
-sudo mount -t ext4 "/dev/"$1"1" /mnt/
-sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/
-PARTUUID_STRING=$(sudo blkid -o value -s PARTUUID "/dev/"$1"1")
-BOOT_ORG='root=/dev/mmcblk0p1'
-BOOT_NEW='root=PARTUUID='$PARTUUID_STRING
-sudo sed -i "s!$BOOT_ORG!$BOOT_NEW!" /boot/extlinux/extlinux.conf
-sudo reboot
-
+echo "******************************************************"
+echo "*            System will Move to $1                  *"
+echo "*   In preparation $1 will be completely erased     *"
+echo "******************************************************"
+printf "Are you sure?[y,n]:"
+	read confirm
+if [ "$confirm" = "y" ]; then
+  sudo parted -s /dev/$1 "mklabel gpt"
+  sudo parted -s /dev/$1 "mkpart primary ext4 1M -1"
+  echo "y" | sudo mkfs -t ext4 "/dev/"$1"1"
+  sudo mount -t ext4 "/dev/"$1"1" /mnt/
+  sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude=/proc / /mnt/
+  PARTUUID_STRING=$(sudo blkid -o value -s PARTUUID "/dev/"$1"1")
+  BOOT_ORG='root=/dev/mmcblk0p1'
+  BOOT_NEW='root=PARTUUID='$PARTUUID_STRING
+  sudo sed -i "s!$BOOT_ORG!$BOOT_NEW!" /boot/extlinux/extlinux.conf
+  sudo reboot
+fi
